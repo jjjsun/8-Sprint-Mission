b1ce25c2f9bfd7b911a65fb31e2f04e85d5e5df6
jsun <ddorri83@naver.com>
2024-06-07 23:57:39 +0900

    스프린트 미션1

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const toDoList = document.querySelector('#to-do-list');
+// const toDoList = document.querySelector('#to-do-list');
 
-function addNewTodo(text) {
-	const newItem = document.createElement('li');
-	newItem.append(text);
-	toDoList.appendChild(newItem)
-}
-
-// 테스트 코드
-addNewTodo('자바스크립트 공부하기');
-addNewTodo('고양이 화장실 청소하기');
-addNewTodo('고양이 장난감 쇼핑하기');
+// function addNewTodo(text) {
+// 	const newItem = document.createElement('li');
+// 	newItem.append(text);
+// 	toDoList.appendChild(newItem)
+// }
